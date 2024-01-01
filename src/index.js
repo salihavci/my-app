e680716e8aa6b1a28fb7b47c65e43f3667dfb80b
salih avcı <casus_salih_16@hotmail.com>
2024-01-01 15:54:20 +0300

    Document external components imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import myExportedData from "./myExportedData";
+function App() {
+  const name = "Bidibidi";
+  return (
+    <div>
+      <h1>Hello react !</h1>
+      <p>
+        This is first react <code>paragraph for {name}.</code>
+      </p>
+      <Pizza />
+      <FilterExportedData />
+    </div>
+  );
+}
 
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinacci" />
+      <h2>Pizza Spinacci</h2>
+      <p>Tomato, mozarella, bidibidi.....</p>
+    </div>
+  );
+}
+
+function FilterExportedData() {
+  let filteredData = myExportedData.find((x) => x.name.includes("Pro"));
+  return <div>{JSON.stringify(filteredData, null, 2)}</div>;
+}
+
+//React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -11,7 +38,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log());
+//React before v18
+// React.render(<App />, document.getElementById("root"));
